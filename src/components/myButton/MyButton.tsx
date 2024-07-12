d305a579203f340cbea161dd39a8c279057272fa
Katja Weimer <katjawmr92@gmail.com>
2024-07-12 09:34:58 +0200

    add

--- src/components/myButton/MyButton.tsx
+++ src/components/myButton/MyButton.tsx
@@ -5,7 +5,7 @@ interface IMyButtonProps {
   type?: 'button' | 'submit' | 'reset'
   onClick?: () => void;
 }
-
+// 
 export default function MyButton({ type='button', onClick, name='default' }:IMyButtonProps) {
     return (
       <button type={type} onClick={onClick} className="myButton">
